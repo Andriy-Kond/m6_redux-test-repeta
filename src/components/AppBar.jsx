21370c0b73d99271f1ea50fde345f2fb9b2925d1
Andrii <andrii.kondratovich@gmail.com>
2024-09-19 10:51:56 +0300

    add: links showing logic

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,16 +3,17 @@ import UserInfo from "./UserInfo";
 import { useSelector } from "react-redux";
 
 function AppBar() {
-  const isSignIn = useSelector(state => state.storeSign.user.isLoggedIn);
+  const isLoggedIn = useSelector(state => state.storeSign.user.isLoggedIn);
 
   return (
     <>
       <header style={{ display: "flex", justifyContent: "space-between" }}>
         <nav style={{ display: "flex", gap: "10px" }}>
           <Link to="/">Home</Link>
-          <Link to="signin">Sign In</Link>
+          {!isLoggedIn && <Link to="signin">Sign In</Link>}
+          {isLoggedIn && <Link to="dashboard">Dashboard</Link>}
         </nav>
-        {isSignIn && <UserInfo />}
+        {isLoggedIn && <UserInfo />}
       </header>
     </>
   );
